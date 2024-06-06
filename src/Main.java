f061526f5c281bf1b89e8cc469942b822563b38f
Dylan <74881467+Cyber-Dylan@users.noreply.github.com>
2024-06-06 15:20:57 +1200

    Added the frame

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+//this is the main file that the suer will run and will call the "Menu" java file.
+public class Main {
+    public static void main(String[] args){
+        new Menu();
+    }
+}
---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+//This is the main menu that will pop up when the game is run.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class Menu {
+    public static void main(String[] args) {
+        //Generates the window.
+        JFrame frame = new JFrame();
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Test Frame");
+        frame.setVisible(true);
+    }
+}
