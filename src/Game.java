ea401adf64343faf790159dd3c701f76b801a1bb
Dylan <74881467+Cyber-Dylan@users.noreply.github.com>
2024-10-15 10:55:51 +1300

    Made the snake move right

--- src/Game.java
+++ src/Game.java
@@ -3,27 +3,47 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.ImageObserver;
-import java.text.AttributedCharacterIterator;
+import java.util.Objects;
 
-public class Game extends JFrame {
+public class Game extends JFrame implements ActionListener, KeyListener {
+
     final int WIDTH = 20; //Grid width
     final int HEIGHT = 20; //Grid height
     final int PIXEL_WIDTH;
     final int PIXEL_HEIGHT;
+    int SNAKE_MOVING_POSITIONX = 3;
+    int SNAKE_MOVING_POSITIONY = 6;
     byte[][] grid = new byte[WIDTH][HEIGHT]; //Creates a grid for the snake.
+    boolean gameOver = false;
+
+    String startingDirection = "right"; //Directions the snake can move.
+
     public Game() {
-        grid[4][6]=1; //Snake starting position.
-        grid[5][2]=2; //Apple starting position.
+        //start();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
         // Generates the window.
         this.setSize(800, 800); // Frame size
-        PIXEL_WIDTH = this.getWidth()/WIDTH;
-        PIXEL_HEIGHT = this.getWidth()/HEIGHT;
+        PIXEL_WIDTH = this.getWidth() / WIDTH;
+        PIXEL_HEIGHT = this.getWidth() / HEIGHT;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false); //Window cannot be resized
         this.setTitle("Snake!"); //Window title
         this.setVisible(true); //Window background color
-    }
+        this.setLayout(null);
+
+        while (!gameOver) {
+            //Move the snake and go to sleep.
+            //gameOver = true;
+            grid[SNAKE_MOVING_POSITIONX][SNAKE_MOVING_POSITIONY] = 1; //Snake starting position.
+            if (Objects.equals(startingDirection, "right") == true) {
+                SNAKE_MOVING_POSITIONX += 1;
+            } //if statement
+        } //while
+
+        } //game class
 
     public void paint(Graphics g) {
         //Generates the grid.
@@ -36,17 +56,31 @@ public class Game extends JFrame {
                     case 1:
                         g.setColor(Color.GREEN); //snake colour
                         break;
-                    case 2:
-                        g.setColor(Color.RED); //Apple Colour
-                        break;
                 }
                 g.fillRect(x*PIXEL_WIDTH, y*PIXEL_HEIGHT, PIXEL_WIDTH, PIXEL_HEIGHT);
             }
         }
     }
 
+
     //Calls the LoseScreen class when the game is over.
     public void actionPerformed(ActionEvent e) {
         new LoseScreen();
+    }
+
+    //Moves the snake.
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
     }
 }
